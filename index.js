d60e2373110333e0e057b468f5a0ae542a0655d1
Varun Kumar Reddy <varunkr9866@gmail.com>
2025-10-31 14:50:59 +0530

    COMPLETED METHOD-2 IN  CLEAN BLOG BY USING EXPRESS

--- index.js
+++ index.js
@@ -15,4 +15,23 @@
 // METHOD-2
 const http = require('http');
 
+let server = http.createServer((req,res) =>{
+    if (req.url === '/about' ) {
+        res.end('About Page');
+        
+    }
+     else if (req.url === '/content' ) {
+        res.end('the Content Page');
+    } 
+    else if (req.url === '/' ) {
+        res.end('the Home Page');
+    } else {
+        res.writeHead(404)
+        res.end('Page Not Found');
+    } 
+});
 
+ server.listen(3300,()=>{
+    console.log('Application is running in port number 3300');
+    
+});
