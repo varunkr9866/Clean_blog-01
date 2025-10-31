48b4d26bc70aec7200f0478f37f46674f0cafeb2
Varun Kumar Reddy <varunkr9866@gmail.com>
2025-10-31 14:43:54 +0530

    COMPLETED CONECTION IN  CLEAN BLOG BY USING EXPRESS

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-const http = require('http')
+const http = require('http');
+
+const server = http.createServer((req,res) =>{
+    console.log(req.url);
+    res.end("Hello Node.js")
+});
+
+server.listen(3300,()=>{
+    console.log('Server is running at port 3300');
+    
+})
